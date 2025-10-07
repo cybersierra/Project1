c326f6795021add18462465c29c7b8d152cea08e
Sierra <sierrastewart.2000@gmail.com>
2025-10-07 18:16:14 -0500

    edits to wish.c

--- wish.c
+++ wish.c
@@ -1,34 +1,7 @@
 #include "wish.h"
 
-// Error message
-static void printError(void) {
-    fputs("An error has occurred\n", stderr);
-    fflush(stderr);
+int main(void){
+    printf("Hello, World!\n");
 
-/* -------- utilities -------- */
-static void err(void) {
-    write(STDERR_FILENO, ERRMSG, sizeof(ERRMSG) - 1);
-    
-}
-
-// Main function
-int main(int arc, char *argv[]) {
-    FILE *in = NULL;
-
-    // Interactive
-    if (argc == 1) {
-        in = stdin;
-
-    // Batch
-    } else if (argc == 2) {
-        in = fopen(argv[1], "r");
-        if (in == NULL) {
-            printError();
-            exit(1);
-        }
-    } else {
-        printError();
-        exit(1);
-    }
-
+    return 0;
 }
